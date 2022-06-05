63e43aa62a6a27a911d48fb88b7d626b5ac17181
Cristiano <55446071+cristiano-93@users.noreply.github.com>
2022-06-05 21:04:57 +0100

    commit 2 5/06
    fixed the to top button and implemented the contact form

--- index.js
+++ index.js
@@ -2,26 +2,12 @@
 function alertFunction() {
   alert("This is not yet properly implemented");
 }
-
-//Get the button:
-topBtn = document.getElementById("topBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
+// Restting the contact form
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
 };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      topBtn.style.display = "block";
-      document.getElementById('topBtn').style.display = 'block';
-  } else {
-    topBtn.style.display = "none";
-  }
-}
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+
